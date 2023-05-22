b55b45f6ff4b3966dfde813a470887088be20789
Дмитрий <brepex@MacBook-Air-Dmitrij.local>
2023-05-22 19:06:56 +0700

    new

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const findNeedItem = require('./untils/findNeedItem')
 const model_buff = require('./models/buff_model')
 
 setInterval(() => {
+    let errors = 0
+    let not_error = 0
     model_buff.fetchPercent2().then(percent_2 => {
         axios({
             url: "https://buff.163.com/api/market/goods?game=csgo&page_num=1&use_suggestion=0",
             method: 'GET'
         }).then(response_goods => {
             model_buff.fetch_error_goods().then(error_goods => {
-                const goods_list_id = response_goods.data.data.items.map(item => item.id).slice(0, 5)
+                const goods_list_id = response_goods.data.data.items.map(item => item.id).slice(0, 7)
 
                 for (const good_id of [...goods_list_id, ...error_goods]) {
                     axios({
@@ -19,7 +21,6 @@ setInterval(() => {
                         model_buff.fetchPercentMain().then(percent_1 => {
 
                             const info_good = findNeedItem(response_good_item_detail.data.data.items.slice(0, 3), percent_1)
-
                             if (info_good) {
                                 model_buff.fetchActiveGood(info_good.id).then(res => {
 
@@ -31,34 +32,45 @@ setInterval(() => {
                                         }).then(trend => {
                                             const trend_value = trend.data.response
 
-                                            if ((parseFloat(info_good.max) - parseFloat(trend_value)) < 0) {
-                                                const percent = (parseFloat(trend_value) - parseFloat(info_good.max)) / info_good.max * 100
+                                            //if ((parseFloat(info_good.max) - parseFloat(trend_value)) < 0) {
 
-                                                if (percent > percent_2) {
-                                                    const max = parseFloat(info_good.max) * 0.975
+                                            const percent = (parseFloat(info_good.max) - parseFloat(trend_value)) / trend_value * 100
 
-                                                    if (info_good.min - max < 0) {
-                                                        const percent_result = (max - parseFloat(info_good.min)) / info_good.min * 100
+                                            if (percent <= percent_2) {
+                                                const max = parseFloat(info_good.max) * 0.975
 
-                                                        if (percent_result >= percent_1) {
-                                                            const message_info = `
-                                ${info_good.info.icon_url}\n\nПредмет: <a href="https://buff.163.com/goods/${good_id}">${response_good_item_detail.data.data.goods_infos[good_id]['market_hash_name']}</a>\nЦена покупки: ${info_good.min}Y\nПрофит: ${info_good.percent}%\nТенденция: ${trend_value}
+
+                                                if (info_good.min - max < 0) {
+                                                    const percent_result = (max - parseFloat(info_good.min)) / info_good.min * 100
+
+
+                                                    if (percent_result >= percent_1) {
+
+                                                        const message_info = `
+                                ${info_good.info.icon_url}\n\nПредмет: <a href="https://buff.163.com/goods/${good_id}">${response_good_item_detail.data.data.goods_infos[good_id]['market_hash_name']}</a>\nЦена покупки: ${info_good.min}Y\nПрофит: ${percent_result}%\nТенденция: ${trend_value}\nИзнос: ${info_good.paintwear}\nЦена продажи: ${info_good.max}(${max})
                             `
-                                                            axios({
-                                                                url: 'http://45.9.43.229:8888',
-                                                                method: 'post',
-                                                                data: {
-                                                                    message: message_info
-                                                                }
-                                                            })
-                                                            model_buff.AddActiveGood(info_good.id)
-                                                        }
+
+                                                        axios({
+                                                            url: 'http://45.9.43.229:8888',
+                                                            method: 'POST',
+                                                            data: {
+                                                                message: message_info
+                                                            }
+                                                        }).then(res => {
+                                                            console.log(res)
+                                                        })
+                                                        model_buff.AddActiveGood(info_good.id).then(res => {
+                                                            console.log("Успешно добавле!!!!")
+                                                        })
                                                     }
                                                 }
                                             }
+                                            //}
                                         }).catch(e => {
-                                            console.log(e.message)
-                                            model_buff.add_good_item(good_id)
+                                            errors = errors + 1
+                                            console.log(errors)
+                                            model_buff.add_good_item(good_id).catch(() => {
+                                            })
                                         })
                                     }
                                 })
@@ -66,7 +78,9 @@ setInterval(() => {
                         })
 
                     }).catch((error) => {
-                        model_buff.add_good_item(good_id)
+                        errors = errors + 1
+                        console.log(errors)
+                        model_buff.add_good_item(good_id).catch(() => {})
                     })
                 }
             })
